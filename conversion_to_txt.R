75f5f61eb14a70f200ac036ec6db52983c724bc6
Amy Yang <yan.yang@student.uts.edu.au>
2022-05-27 16:39:38 +1000

    Add comments for R script - Comment on the R script regarding coversion from docx to txt files

--- conversion_to_txt.R
+++ conversion_to_txt.R
@@ -2,10 +2,13 @@
 library(tm)
 library(textreadr)
 
+# Read the documents in the folder
 docs_list <- read_dir("./Data/Interviews_part1&2")
 
+# Extract the unique list for document names
 list <- unique(docs_list$document)
 
+# Create function to read docx documents and save into txt files
 for (name in list){
   docs <- read_docx(paste0("./Data/Interviews_part1&2/",name,".docx"))
   joined_docs <- paste(docs, collapse = '')                 
