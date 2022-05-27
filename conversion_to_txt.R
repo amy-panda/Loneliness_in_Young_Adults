0b980cf9892fd35ab59cef6ece8fab4b71e1e0ac
Amy Yang <yan.yang@student.uts.edu.au>
2022-05-27 22:48:52 +1000

    update R file

--- conversion_to_txt.R
+++ conversion_to_txt.R
@@ -1,5 +1,5 @@
 # Load the library
-library(tm)
+# library(tm)
 library(textreadr)
 
 # Read the documents in the folder
@@ -11,6 +11,6 @@ list <- unique(docs_list$document)
 # Create function to read docx documents and save into txt files
 for (name in list){
   docs <- read_docx(paste0("./Data/Interviews_part1&2/",name,".docx"))
-  joined_docs <- paste(docs, collapse = '')                 
+  joined_docs <- paste(docs,collapse = " ")                 
   write.table(joined_docs,file=paste0(name," - part1&2.txt"),sep="\t",row.names=FALSE)
 }
